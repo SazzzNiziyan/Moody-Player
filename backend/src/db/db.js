d426e42e7da2063b0e2dff8fe85a9d99d88c6fb4
Sajal Singh <as3199432@gmail.com>
2025-07-17 20:57:11 +0530

    connect to databas and start creating the song schema

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,6 +1,14 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 function connectDB() {
     
-    mongoose.connect("")
+    mongoose.connect(process.env.MONGODB_URL)
+    .then(()=>{
+       console.log('Connected to MongoDB');
+    })
+    .catch((err)=>{
+        console.error('Error connecting to MongoDB:', err);
+    });
 }
+
+module.exports = connectDB;
