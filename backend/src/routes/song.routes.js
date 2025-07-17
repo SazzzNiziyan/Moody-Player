ffa373c8b0bff1a02660969b4a537e98256be464
Sajal Singh <as3199432@gmail.com>
2025-07-17 23:01:48 +0530

    completed the connection of database and server and also the api is also working fine the data req. now successfully completed

---
+++ backend/src/routes/song.routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const multer = require('multer');
+const uploadFile = require('../service/storage.service')
+
+
+const router = express.Router();
+
+const upload = multer({storage:multer.memoryStorage()});
+
+
+router.post('/song',upload.single("audio"),async(req,res)=>{
+    console.log(req.body);
+    console.log(req.file);
+    const fileData = await uploadFile(req.file);
+    res.status(201).json({
+        message: 'Song created Successfully',
+        song: req.body
+    })
+})
+
+module.exports = router;
